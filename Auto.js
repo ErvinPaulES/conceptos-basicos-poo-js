7e9a457ead68a038f3d341ff2356d911b887dec0
ErvinPaulES <eescobar1@ucol.mx>
2019-02-08 15:29:33 -0600

    Actualizado

--- Auto.js
+++ Auto.js
@@ -1,9 +1,38 @@
-class Autos{
-    constructor( marca, modelo, color, kilometraje){
-        this.marca=marca
-        this.modelo=modelo
-        this.color=color
-        this.kilometraje=kilometraje
-        this.estado="Apagado"
+export default class Auto{
+    constructor( marca, modelo, color="Azul", kilometraje=0){
+        this._marca=marca
+        this._modelo=modelo
+        this._color=color
+        this._kilometraje=kilometraje
+        this._estado="Apagado"
+    }
+    get color(){
+        return this._color
+    }
+    set color(color){
+        this._color=color.toUpperCase()
+
+    }
+    mostrarEstado(){
+        console.log(`El auto ${this._marca} ${this._color} del año ${this._modelo} 
+        tiene un kilometraje de ${this._kilometraje}kms y esta ${this._estado}`)
+    }
+    encender(){
+        this._estado="Encendido"
+        this.mostrarEstado()
+    }
+    apagar(){
+        this._estado="Apagado"
+        this.mostrarEstado()
+    }
+    Avanzar(velocidad, tiempo){
+        if(this._estado==="Apagado"){
+            console.log("El auto esta apagado por consecuente no puede avanzar")
+        }
+        else{
+            let distancia =velocidad*tiempo
+            this._kilometraje=this._kilometraje+distancia
+            this.mostrarEstado()
+        }
     }
 }
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hola, mundo")
+import Auto from "./Auto.js"
+let auto1 = new Auto("Ford", 2000, "rojo", 100)
+let auto2= new Auto("Nissan", 2018, "blanco")
+let auto3 = new Auto("VW", 2010)
+
+console.log(`Color ${auto1.color}`)
+console.log(`Color ${auto2.color}`)
+auto1.color="Amarillo"
+auto2.color="Azul"
+
+auto1.mostrarEstado()
+auto1.encender()
+auto1.Avanzar(50, 2)
+auto1.apagar()
+auto1.Avanzar(20, 1)
+
+auto2.encender()
+auto3.encender()
